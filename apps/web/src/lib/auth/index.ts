dc9c5aefc9965422534417dabb3ecebfe784e3db
Aldo R. L. <aldo.ruiz.luna@gmail.com>
2026-05-27 00:53:06 -0600

    fix: export Auth.js handlers

--- apps/web/src/lib/auth/index.ts
+++ apps/web/src/lib/auth/index.ts
@@ -1,9 +1,4 @@
 import NextAuth from 'next-auth'
 import { authConfig } from './config'
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth(authConfig)
+export const { handlers, auth, signIn, signOut } = NextAuth(authConfig)
